41d07c479f0dfe38869d6c9cfd1f8f5db1ab1dfb
Arciiix <artur.nowak100@gmail.com>
2023-03-26 19:50:49 +0200

    Create basic mock

---
+++ mock/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+
+const PORT = 3823;
+
+const app = express();
+
+app.get("", (req: Request, res: Response) => {
+  res.send("Hello world - the mock works!");
+});
+
+app.get("/v1/school/status", (req: Request, res: Response) => {
+  // TODO: Make it dynamic
+  res.send({
+    IS_RUNNING: true,
+    TEMPERATURE_MAIN: {
+      value: 3039,
+      error: false,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Mock has started listening on: ${PORT}`);
+});
